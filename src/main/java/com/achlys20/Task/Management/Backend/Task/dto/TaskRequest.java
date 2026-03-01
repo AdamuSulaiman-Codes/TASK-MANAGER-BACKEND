4434d8000755766212a842ddc2962be9671ae865
adamusulaimanmuhammad <sulaimancodes2235@gmail.com>
2026-03-01 16:03:53 +0100

    fixed the bug in my taskController and service

--- src/main/java/com/achlys20/Task/Management/Backend/Task/dto/TaskRequest.java
+++ src/main/java/com/achlys20/Task/Management/Backend/Task/dto/TaskRequest.java
@@ -4,11 +4,13 @@ import com.achlys20.Task.Management.Backend.Task.Priority;
 import com.achlys20.Task.Management.Backend.Task.Status;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class TaskRequest {
     private Long id;
     private String title;
